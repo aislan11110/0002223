8ecdf25ee971d1f0488819a1f903ba63ed68d1bb
aislan <agos@ecomp.poli.br>
2022-04-13 11:45:20 -0300

    q isso

---
+++ src/main/kotlin/71A.kt
@@ -0,0 +1,21 @@
+
+
+fun receberStr(s:String):String{
+    return if(s.length>10){
+        s[0]+(s.length-2).toString()+s[s.length-1]
+    } else {
+        s
+    }
+
+}
+
+
+
+fun main (){
+    var x = readln().toInt()
+    var y = 0
+    while(y!=x){
+        println(receberStr(readln()))
+        y++
+    }
+}
